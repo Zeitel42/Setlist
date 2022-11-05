53e3d4a46ed7a28a41476176a2085dd18c182d9e
Zeitel42 <43343991+Zeitel42@users.noreply.github.com>
2022-11-05 09:10:45 -0400

    initial commit react app created

---
+++ setlist/src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Set List Game</h1>
+      </header>
+      <button>Play</button>
+      <p>This will be the app</p>
+    </div>
+  );
+}
+
+export default App;
